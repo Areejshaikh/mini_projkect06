88bbda13c2c43e97454d0287ce0bcb1f6d7b149f
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-12 00:48:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// Q7: Write a simple number 
+// guessing game. Provide a selected number 
+// and a guess. Based on those numbers, give 
+// players clues if their guess is higher, lower, or correct.
+let randomNumber = 2;
+// import inquirer from "inquirer";
+// console.log("Here is a Number gussing  game");
+// const randomNumber = Math.floor(Math.random() * 2 + 1);
+// const answer = await inquirer.prompt
+// (
+//     [
+//         {
+//             name :"guessNumber",
+//             message: "Please guess a number between 1 to 6:",
+//             type: "input"
+//         },
+//     ]
+// )
+// if (randomNumber === answer.guessNumber) {
+if (randomNumber === 2) {
+    console.log("Congratulations! You Guessd Right Number");
+}
+else {
+    console.log("You guessd rong number");
+}
+export {};
---
+++ index.ts
@@ -0,0 +1,39 @@
+// Q7: Write a simple number 
+// guessing game. Provide a selected number 
+// and a guess. Based on those numbers, give 
+// players clues if their guess is higher, lower, or correct.
+
+import inquirer from "inquirer";
+console.log("Here is a Number gussing  game");
+const randomNumber = Math.floor(Math.random() * 2 + 1);
+const answer = await inquirer.prompt
+(
+    [
+        {
+            name :"guessNumber",
+            message: "Please guess a number between 1 to 6:",
+            type: "input"
+        },
+    ]
+)
+if (randomNumber === answer.guessNumber) {
+    if (randomNumber === 2 ) {
+    console.log("Congratulations! You Guessd Right Number");
+}
+else {
+    console.log("You guessd rong number");
+}
+};
+
+
+
+// Example 2
+
+// let randomNum = 2
+
+// if (randomNum === 2 ) {
+//     console.log("Congratulations! You Guessd Right Number");
+// }
+// else {
+//     console.log("You guessd rong number");
+// }
